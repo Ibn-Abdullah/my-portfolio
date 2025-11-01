f2783c55dd5283cb32b6cf205f7b6b854eb39cdc
Hazem Abdallah <hjaballah244@gmail.com>
2025-11-01 20:51:36 +0200

    react analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { inject } from "@vercel/analytics";
-
+import { Analytics } from "@vercel/analytics/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<App />
+		<Analytics />
 	</React.StrictMode>
 );
-
-inject();
